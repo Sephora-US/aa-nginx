cd406d0c10f4c0af37a0a58cd8334242cea43421
Kiran Yayavaram <kiran.yayavaram@sephora.com>
2023-11-17 08:52:04 -0800

    more updates after demo

---
+++ static/home.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "You are in Google Cloud!";
+}
+
+function resetFunction() {
+  document.getElementById("demo").innerHTML = "Where are you?";
+}
